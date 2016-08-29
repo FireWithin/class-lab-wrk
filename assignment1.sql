5fc64ddcb3f4d0747c3fa8f0466f14a21e13c841
Aditya Jain <adityajain.aj10@gmail.com>
2016-08-29 11:10:46 +0530

    Update assignment1.sql

--- assignment1.sql
+++ assignment1.sql
@@ -61,3 +61,18 @@ select count(OrderID) from sales;
 select count(distinct(CustomerName)) from sales;
 /*Write a query to find the sum of all orders?*/
 select sum(OrderPrice) from sales;
+/*Write a query to find how many items have been sold in total?*/
+select sum(OrderQuantity) from sales;
+/*Write a query to find the average number of items per order?*/
+select avg(OrderQuantity) from sales;
+/*Write a query to return the average OrderQuantity for all orders with OrderPrice greater than 200?*/
+select avg(OrderQuantity) from sales where OrderPrice>200;
+/*Write a query to find out what was the minimum price paid for any of the orders in the Sales table?*/
+select min(OrderPrice) from sales;
+/*Write a query to return the highest OrderPrice from the Sales table?*/
+select max(OrderPrice) from sales;
+/*Write a query to retrieve a list with unique customers from Sales table, and to get the total amount 
+each customer has spent in store?*/
+select CustomerName,sum(OrderPrice*OrderQuantity) from sales group by CustomerName;
+/*Write a query to select all unique customers, who have spent more than 1200 in store*/
+select distinct(ii.cn) from (select CustomerName as cn,sum(OrderPrice*OrderQuantity) as ss from sales group by CustomerName) as ii where ii.ss>1200;
